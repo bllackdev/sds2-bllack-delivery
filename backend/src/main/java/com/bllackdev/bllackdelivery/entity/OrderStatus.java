2a20fede0a6a3cab1d12c0a916c37208b7a23ba6
Raphael Renato Medeiros da Matta <CIANDT\raphaelrenato@lnb028484cps.ciandt.global>
2021-01-16 13:51:05 -0300

    Domain model

---
+++ backend/src/main/java/com/bllackdev/bllackdelivery/entity/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.bllackdev.bllackdelivery.entity;
+
+public enum OrderStatus {
+
+    PENDING, DELIVERED;
+}
---
+++ backend/src/main/java/com/bllackdev/bllackdelivery/entity/Product.java
@@ -0,0 +1,96 @@
+package com.bllackdev.bllackdelivery.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+    private String imageUrl;
+    private String description;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, Double price, String description, String imageUrl) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null)
+            return false;
+        if (getClass() != o.getClass())
+            return false;
+        Product other = (Product) o;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+}
